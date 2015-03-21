2ca564c7254d55829bfa6429e75bc915af6706a3
Daniel Fabian <daniel.fabian@integral-it.ch>
2015-03-21 19:16:41 +0100

    Add first problem.

--- PicAPixSolver.Web/Solver.fs
+++ PicAPixSolver.Web/Solver.fs
@@ -1,5 +1,41 @@
 ﻿module PicAPixSolver.Web.Solver
 
+type Color = byte * byte * byte
+type Number = int * Color
+
+type Problem =
+    { Vertical : Number list list
+      Horizontal : Number list list }
+
+
+let asBlack numbers =
+    let black = 0uy, 0uy, 0uy
+    
+    numbers
+    |> List.map (List.map (fun n -> n, black)) 
+
+let problem1 =
+    let vertical =
+        [
+            [ 3 ]
+            [ 1; 1 ]
+            [ 2; 2 ]
+            [ 1; 1 ]
+            [ 3 ]
+        ]
+
+    let horizontal =
+        [
+            [ 3 ]
+            [ 1; 1; 1 ]
+            [ 1; 1 ]
+            [ 1; 1; 1 ]
+            [ 3 ]
+        ]
+
+    { Vertical = asBlack vertical
+      Horizontal = asBlack horizontal }
+
 let defaultModel =
     let x, y = 30, 40
     Array2D.init x y (fun i j ->
